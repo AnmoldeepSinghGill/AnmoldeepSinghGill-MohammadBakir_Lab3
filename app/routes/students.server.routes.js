202dba48f1800e6af7af84c8407afe63bf64623a
Mohammad Bakir <Bakir-md@outlook.co.il>
2021-03-18 04:46:42 +0200

    added JWT Token for authentication, authenticate, getErrorMessage, verfiyUser, and welcome methods

--- app/routes/students.server.routes.js
+++ app/routes/students.server.routes.js
@@ -2,4 +2,7 @@
 var students = require("../controllers/students.server.controller");
 
 //handle routing for get and post request
-module.exports = function (app) {};
+module.exports = function (app) {
+    // app.post('/signin', users.authenticate);
+    // app.get('/welcome',users.welcome);
+};
