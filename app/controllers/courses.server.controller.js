cf472ae65e4c9c2f074035457da8f5c3a4c2a35e
Mohammad Bakir <Bakir-md@outlook.co.il>
2021-03-20 21:25:39 +0200

    added update course

--- app/controllers/courses.server.controller.js
+++ app/controllers/courses.server.controller.js
@@ -1,41 +1,47 @@
 const Course = require("mongoose").model("Course");
 
-exports.addCourse = (req, res) => {
-  const course = new Course(req.body);
-  console.log(req.body);
+exports.addCourse = (req, res, next) => {
+    const course = new Course(req.body);
+    console.log(req.body);
 
-  // Try saving the new Course  document
-  course.save((err, course) => {
-    // If an error occurs, use flash messages to report the error
-    if (err) {
-      // Use the error handling method to get the error message
-      const message = getErrorMessage(err);
-      console.log(message);
-    }
+    course.save((err, course) => {
+        if (err) {
+            return next(err);
+        }
+        res.status(200).send({course: course});
+    });
+};
 
-    // Redirect the Student  back to the main application page
-    res.status(200).send({ course: course });
-  });
+exports.updateCourse = (req, res, next) => {
+    const course = new Course(req.body);
+    console.log(req.body);
+
+    Course.findByIdAndUpdate(req.params.courseId, course, function (err, co) {
+        if (err) {
+            return next(err);
+        }
+        res.status(200).send(co);
+    });
 };
 
 exports.listAllCourses = (req, res) => {
-  Course.find({}, (err, courses) => {
-    if (err) {
-      return next(err);
-    } else {
-      console.log("courses", courses);
-      res.status(200).send(courses);
-    }
-  });
+    Course.find({}, (err, courses) => {
+        if (err) {
+            return next(err);
+        } else {
+            console.log("courses", courses);
+            res.status(200).send(courses);
+        }
+    });
 };
 
 exports.listAllCourses = (req, res) => {
-  Course.find({}, (err, courses) => {
-    if (err) {
-      return next(err);
-    } else {
-      console.log("courses", courses);
-      res.status(200).send(courses);
-    }
-  });
+    Course.find({}, (err, courses) => {
+        if (err) {
+            return next(err);
+        } else {
+            console.log("courses", courses);
+            res.status(200).send(courses);
+        }
+    });
 };
--- app/routes/courses.server.routes.js
+++ app/routes/courses.server.routes.js
@@ -4,5 +4,6 @@ var courses = require("../controllers/courses.server.controller");
 //handle routing for get and post request
 module.exports = function (app) {
   app.post("/course", courses.addCourse);
+  app.put("/course/:courseId", courses.updateCourse);
   app.get("/courses", courses.listAllCourses);
 };
