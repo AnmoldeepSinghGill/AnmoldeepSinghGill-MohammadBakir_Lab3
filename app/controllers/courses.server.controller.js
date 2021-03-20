f618655a172d97cd9fcfe0fe8707ece22ef68a45
Anmoldeep Singh Gill <gillasg788@gmail.com>
2021-03-19 20:15:49 -0400

    Added Add course and List all courses

---
+++ app/controllers/courses.server.controller.js
@@ -0,0 +1,30 @@
+const Course = require("mongoose").model("Course");
+
+exports.addCourse = (req, res) => {
+  const course = new Course(req.body);
+  console.log(req.body);
+
+  // Try saving the new Course  document
+  course.save((err, course) => {
+    // If an error occurs, use flash messages to report the error
+    if (err) {
+      // Use the error handling method to get the error message
+      const message = getErrorMessage(err);
+      console.log(message);
+    }
+
+    // Redirect the Student  back to the main application page
+    res.status(200).send({ course: course });
+  });
+};
+
+exports.listAllCourses = (req, res) => {
+  Course.find({}, (err, courses) => {
+    if (err) {
+      return next(err);
+    } else {
+      console.log("courses", courses);
+      res.status(200).send(courses);
+    }
+  });
+};
--- app/routes/courses.server.routes.js
+++ app/routes/courses.server.routes.js
@@ -2,4 +2,7 @@
 var courses = require("../controllers/courses.server.controller");
 
 //handle routing for get and post request
-module.exports = function (app) {};
+module.exports = function (app) {
+  app.post("/course", courses.addCourse);
+  app.get("/courses", courses.listAllCourses);
+};
